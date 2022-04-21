2dde86026683a7bf975ad4dca92984c5e08af0e9
Milind Dalvi <milind.dalvi14@gmail.com>
2022-04-22 04:31:36 +0530

    new method list_files in s3 and new rounding methods in math module

--- toolbox/aws/s3/util.py
+++ toolbox/aws/s3/util.py
@@ -8,7 +8,7 @@ def upload_file(bucket_name, source_path, destination_path, file_name):
     :param source_path: Fully qualified path including file name at source
     :param destination_path: destination path without bucket name and file name
     :param file_name: Desired named of file at destination
-    :return:
+    :return: None
     """
     s3_resource = boto3.resource('s3')
     s3_resource.Object(bucket_name, destination_path + file_name).upload_file(source_path)
@@ -21,7 +21,18 @@ def download_file(bucket_name, source_path, destination_path, file_name):
     :param source_path: Fully qualified path including file name at source
     :param destination_path: destination path to save file
     :param file_name: Desired named of file at destination
-    :return:
+    :return: None
     """
     s3_resource = boto3.resource('s3')
     s3_resource.meta.client.download_file(bucket_name, source_path, destination_path + file_name)
+
+
+def list_files(bucket_name, source_path):
+    """
+    :param bucket_name: Name of bucket (e.g. s3-my-bucket)
+    :param source_path: S3 key without the bucket name
+    :return: list
+    """
+    s3 = boto3.resource("s3")
+    s3_bucket = s3.Bucket(bucket_name)
+    return [f.key for f in s3_bucket.objects.filter(Prefix=source_path).all()]
--- toolbox/math/util.py
+++ toolbox/math/util.py
@@ -10,3 +10,14 @@ def get_school_round(a_in, n_in):
         return round(a_in + 1 / 10 ** (n_in + 1), n_in)
     else:
         return round(a_in, n_in)
+
+
+def round_nearest(num: float, to: float) -> float:
+    """
+    Credited to Paul H.
+    https://stackoverflow.com/questions/28425705/python-round-a-float-to-nearest-0-05-or-to-multiple-of-another-float
+    :param num:
+    :param to:
+    :return: float
+    """
+    return round(num / to) * to
