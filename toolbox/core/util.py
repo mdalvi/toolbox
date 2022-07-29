302bb49219f0f6613340f5c0b5011db1385342f3
Milind Dalvi <milind.dalvi14@gmail.com>
2022-07-29 16:36:18 +0530

    core and deep learning module updates

--- toolbox/core/util.py
+++ toolbox/core/util.py
@@ -10,3 +10,23 @@ def get_class_from_string(module_name: str, class_name: str):
     """
     module = importlib.import_module(module_name)
     return getattr(module, class_name)
+
+
+def get_duplicates_in_list(lst):
+    """
+    https://stackoverflow.com/questions/9835762/how-do-i-find-the-duplicates-in-a-list-and-create-another-list-with-them
+    """
+    seen1 = set()
+    seen2 = set()
+
+    seen1_add = seen1.add
+    seen2_add = seen2.add
+
+    for item in lst:
+        if item in seen1:
+            seen2_add(item)
+        else:
+            seen1_add(item)
+
+    return list(seen2)
+
